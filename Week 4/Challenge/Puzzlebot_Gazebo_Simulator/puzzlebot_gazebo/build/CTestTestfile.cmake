835c87b5013ed282b658f30dd9044892d0228b26
Mario Martinez <67285979+mariomartinezgro@users.noreply.github.com>
2024-03-25 12:08:10 -0600

    init

--- Week 4/Challenge/Puzzlebot_Gazebo_Simulator/puzzlebot_gazebo/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liangyichen/catkin_ws/src/puzzlebot/puzzlebot_gazebo
-# Build directory: /home/liangyichen/catkin_ws/src/puzzlebot/puzzlebot_gazebo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
